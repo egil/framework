2359ce1f9da4567f0c758b2d86bf2dcfd186ca96
Egil Hansen <egil@assimilated.dk>
2025-07-05 13:53:30 +0000

    remove: previous implementation

--- Egil.Orleans.EventSourcing/src/Egil.Orleans.EventSourcing/IEventStorageProvider.cs
+++
@@ -1,8 +0,0 @@
-using Orleans.Runtime;
-
-namespace Egil.Orleans.EventSourcing;
-
-public interface IEventStorageProvider
-{
-    IEventStorage<TEvent> Create<TEvent>(IGrainContext grainContext);
-}
