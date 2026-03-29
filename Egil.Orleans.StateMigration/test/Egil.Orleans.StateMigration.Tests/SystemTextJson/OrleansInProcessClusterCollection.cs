d084488d30a7fe69813ebd9278fc27d5fa76f0f7
Egil Hansen <egil@assimilated.dk>
2026-03-29 01:18:49 +0000

    chore: remove previous experimental implementation

--- Egil.Orleans.StateMigration/test/Egil.Orleans.StateMigration.Tests/SystemTextJson/OrleansInProcessClusterCollection.cs
+++
@@ -1,7 +0,0 @@
-namespace Egil.Orleans.StateMigration.Tests.SystemTextJson;
-
-[CollectionDefinition(Name)]
-public sealed class OrleansInProcessClusterCollection : ICollectionFixture<OrleansInProcessClusterFixture>
-{
-    public const string Name = "OrleansInProcessCluster";
-}
